cd62dcf89182878931268af4912e80f651e48987
Mohamed Ramadan <MoRm9n@gmail.com>
2022-11-04 22:54:49 +0200

    add products

--- lib/Screens/grocery.dart
+++ lib/Screens/grocery.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:gfresh/helpers/helpers.dart';
 import 'package:gfresh/widgets/category_widget.dart';
-import 'package:gfresh/widgets/deals_widget.dart';
 
+import '../config/app_color.dart';
 import '../providers/data_provider.dart';
 import '../widgets/address_widget.dart';
+import '../widgets/deals_widget.dart';
 
 class GroceryScreen extends StatefulWidget {
   const GroceryScreen({super.key});
@@ -18,106 +20,111 @@ class _GroceryScreenState extends State<GroceryScreen> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    Color appColor = AppColor.kColorList.getRandomValue();
     return SingleChildScrollView(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          //* Search bar
-          Container(
-            margin: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 1,
-                color: Colors.grey,
+      child: Obx(
+        () {
+          if (controller.isLoading.value == true) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              //* Search bar
+              Container(
+                margin: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.grey,
+                  ),
+                  color: const Color(0xffF5F7F9),
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    prefixIcon: Icon(Icons.search),
+                    hintText: "Search in Thousands of Products",
+                  ),
+                ),
               ),
-              color: const Color(0xffF5F7F9),
-              borderRadius: const BorderRadius.all(Radius.circular(10)),
-            ),
-            child: const TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                prefixIcon: Icon(Icons.search),
-                hintText: "Search in Thousands of Products",
-              ),
-            ),
-          ),
-          //* Saved Addresses
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: SizedBox(
-              width: size.width,
-              height: size.height * 0.1 < 80 ? 100 : size.height * 0.1,
-              child: Obx(
-                () {
-                  if (controller.isLoading.value == true) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  return ListView.builder(
+              //* Saved Addresses
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: SizedBox(
+                  width: size.width,
+                  height: size.height * 0.1 < 80 ? 100 : size.height * 0.1,
+                  child: ListView.builder(
                     itemCount: controller.getLocation.length,
                     shrinkWrap: true,
                     scrollDirection: Axis.horizontal,
                     itemBuilder: (context, index) => AddressWidget(
                       uLocation: controller.getLocation[index],
                     ),
-                  );
-                },
-              ),
-            ),
-          ),
-          //* Saved Category
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Explore by Category",
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        setState(() {});
-                      },
-                      child: Text(
-                        "See All",
-                        style: Theme.of(context)
-                            .textTheme
-                            .subtitle2!
-                            .copyWith(color: Colors.grey),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  width: size.width,
-                  height: size.height * 0.13 < 100 ? 120 : size.height * 0.13,
-                  child: Obx(
-                    () {
-                      if (controller.isLoading.value == true) {
-                        return const Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                      return ListView.builder(
-                        itemCount: controller.getCategories.length,
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (context, index) => CategoryWidget(
-                          category: controller.getCategories[index],
-                        ),
-                      );
-                    },
                   ),
                 ),
-              ],
-            ),
-          ),
-          const DealsOfTheDay(),
-        ],
+              ),
+              //* Saved Category
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Explore by Category",
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {});
+                          },
+                          child: Text(
+                            "See All",
+                            style: Theme.of(context)
+                                .textTheme
+                                .subtitle2!
+                                .copyWith(color: Colors.grey),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                        width: size.width,
+                        height:
+                            size.height * 0.13 < 100 ? 120 : size.height * 0.13,
+                        child: ListView.builder(
+                          itemCount: controller.getCategories.length,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) => CategoryWidget(
+                            category: controller.getCategories[index],
+                          ),
+                        )),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: SizedBox(
+                  height: size.height * 0.18 < 130 ? 130 : size.height * 0.18,
+                  width: size.width,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: controller.getProducts.length,
+                    itemBuilder: (context, index) => DealsOfTheDay(
+                      product: controller.getProducts[index],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          );
+        },
       ),
     );
   }
--- lib/widgets/deals_widget.dart
+++ lib/widgets/deals_widget.dart
@@ -1,7 +1,14 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gfresh/config/app_color.dart';
+import 'package:gfresh/helpers/helpers.dart';
+import 'package:gfresh/models/product.dart';
 
 class DealsOfTheDay extends StatefulWidget {
-  const DealsOfTheDay({super.key});
+  final Product product;
+  const DealsOfTheDay({
+    super.key,
+    required this.product,
+  });
 
   @override
   State<DealsOfTheDay> createState() => _DealsOfTheDayState();
@@ -10,11 +17,96 @@ class DealsOfTheDay extends StatefulWidget {
 class _DealsOfTheDayState extends State<DealsOfTheDay> {
   @override
   Widget build(BuildContext context) {
+    final product = widget.product;
     final size = MediaQuery.of(context).size;
-    return SizedBox(
-      height: size.height * 0.3 < 200 ? 200 : size.height * 0.3,
-      width: size.width,
-      child: Container(),
+    final appColor = AppColor.kColorList.getRandomValue();
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 10,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Stack(
+            children: [
+              Container(
+                width: 110,
+                decoration: BoxDecoration(
+                  color: appColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Positioned(
+                  left: 0,
+                  top: 0,
+                  child: IconButton(
+                    onPressed: () => setState(() {
+                      product.favorite = !product.favorite;
+                    }),
+                    icon: Icon(
+                      product.favorite
+                          ? Icons.favorite
+                          : Icons.favorite_outline_outlined,
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                product.title,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Text(
+                "Places${product.places}",
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(Icons.location_on_outlined),
+                  Text(
+                    product.address,
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                ],
+              ),
+              RichText(
+                text: TextSpan(
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: ' \$ ${product.dPrice}   ',
+                        style: const TextStyle(
+                            color: Colors.red, fontWeight: FontWeight.bold)),
+                    TextSpan(
+                      text: '\$${product.oPrice}',
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        decoration: TextDecoration.lineThrough,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
